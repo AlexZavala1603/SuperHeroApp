1d0127d8d957b5d933fe31d4ab3b01af138bfdc0
Alejandro Zavala <85930734+AlexZavala1603@users.noreply.github.com>
2022-02-25 15:57:05 -0600

    The "Character" model is added to contain the superhero information.

---
+++ app/src/main/java/com/example/superheroapp/model/Character.kt
@@ -0,0 +1,17 @@
+package com.example.superheroapp.model
+
+/*
+* @param id (int, optional): The unique ID of the character resource.
+* @param name (string, optional): The name of the character.
+* @param description (string, optional): A short bio or description of the character.
+* @param modified (Date, optional): The date the resource was most recently modified.
+* @param thumbnail (Image, optional): The representative image for this character.
+*/
+
+data class Character(
+    var id:Int,
+    var name:String,
+    var description: String,
+    var modified: String,
+    var thumbnail: Thumbnail
+)
