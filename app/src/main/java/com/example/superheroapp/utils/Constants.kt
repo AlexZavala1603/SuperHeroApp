f0f9d78ab672189784095ecf3a9a7bf5c3141339
Alejandro Zavala <85930734+AlexZavala1603@users.noreply.github.com>
2022-02-25 17:14:54 -0600

    A class is added to store constants.

---
+++ app/src/main/java/com/example/superheroapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.superheroapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://gateway.marvel.com/"
+        const val TS = "1"
+        const val API_KEY = "842096b471ab0751ecff55b37374a1c4"
+        const val HASH = "3f333e7614530f393ed737d3988e7a7d"
+    }
+}
