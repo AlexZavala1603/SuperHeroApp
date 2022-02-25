563a9f34eafcaf5fbb4a8a8d467cb479b99a0585
Alejandro Zavala <85930734+AlexZavala1603@users.noreply.github.com>
2022-02-25 17:36:41 -0600

    Added adapter for recyclerview and character items.

---
+++ app/src/main/java/com/example/superheroapp/data/adapters/CharactersAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.superheroapp.data.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.superheroapp.model.Character
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.superheroapp.R
+import com.example.superheroapp.databinding.ItemCharacterBinding
+
+class CharactersAdapter : RecyclerView.Adapter<CharactersAdapter.CharactersHolder>() {
+
+    var characters = mutableListOf<Character>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setCharactersList(characters: List<Character>){
+        this.characters.addAll(characters)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharactersHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return CharactersHolder(layoutInflater.inflate(R.layout.item_character, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: CharactersHolder, position: Int) {
+        val item: Character = characters[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = characters.size
+
+    class CharactersHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        private val binding = ItemCharacterBinding.bind(itemView)
+
+        fun bind(item: Character) {
+            binding.cardCharacter.setOnClickListener {
+                Toast.makeText(itemView.context, "Clikeado!", Toast.LENGTH_SHORT).show()
+            }
+
+            binding.txtName.text = item.name
+            binding.txtDate.text = item.modified
+
+            val options: RequestOptions = RequestOptions().centerCrop().placeholder(R.drawable.default_placeholder).error(R.drawable.error_placeholder)
+            val url = item.thumbnail.path + "."+ item.thumbnail.extension
+
+            Glide.with(itemView.context)
+                .load(url)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .apply(options)
+                .into(binding.imgThumbnail)
+        }
+    }
+
+}
