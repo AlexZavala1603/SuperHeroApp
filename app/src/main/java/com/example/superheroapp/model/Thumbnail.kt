7fffd104f53fa168659337e3534ffd242f779238
Alejandro Zavala <85930734+AlexZavala1603@users.noreply.github.com>
2022-02-25 16:03:58 -0600

    The "Thumbnail" model is added to contain the information of the character image.

---
+++ app/src/main/java/com/example/superheroapp/model/Thumbnail.kt
@@ -0,0 +1,11 @@
+package com.example.superheroapp.model
+
+/*
+* path (string, optional): The directory path of to the image.
+* extension (string, optional): The file extension for the image.
+*/
+
+data class Thumbnail(
+    var path : String,
+    var extension: String
+)
