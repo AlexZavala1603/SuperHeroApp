927a54437a5d84db1a7d66d800bb703ac418c4bb
Alejandro Zavala <85930734+AlexZavala1603@users.noreply.github.com>
2022-02-25 19:58:30 -0600

    Implementation on OnItemClickListener in CharactersFragment.kt

--- app/src/main/java/com/example/superheroapp/view/CharactersFragment.kt
+++ app/src/main/java/com/example/superheroapp/view/CharactersFragment.kt
@@ -1,22 +1,25 @@
 package com.example.superheroapp.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.core.widget.NestedScrollView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.superheroapp.R
 import com.example.superheroapp.data.adapters.CharactersAdapter
 import com.example.superheroapp.databinding.FragmentCharactersBinding
+import com.example.superheroapp.model.Character
+import com.example.superheroapp.utils.OnItemClickListener
 import com.example.superheroapp.viewmodel.CharactersViewModel
 
 class CharactersFragment : Fragment() {
 
     private lateinit var binding: FragmentCharactersBinding
-    private val adapter: CharactersAdapter = CharactersAdapter()
     private val viewModel: CharactersViewModel by viewModels()
     private var currentPage = 0
 
@@ -31,10 +34,19 @@ class CharactersFragment : Fragment() {
     }
 
     private fun init() {
+
+     val adapter = CharactersAdapter(object : OnItemClickListener{
+            override fun onItemClick(item: Character) {
+                val bundle = Bundle()
+                bundle.putSerializable("character", item)
+                view?.let { Navigation.findNavController(it).navigate(R.id.action_charactersFragment_to_charactersDetailedFragment, bundle) }
+            }
+        })
+
         binding.fabBack.setOnClickListener { activity?.onBackPressed() }
 
         binding.recyclerSuperheros.apply {
-            adapter = this@CharactersFragment.adapter
+            this.adapter = adapter
             layoutManager = GridLayoutManager(context, 2)
         }
 
